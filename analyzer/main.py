b31cd192a331fa29218b9dc9e001dc960448f36c
於杰 <alexkie@yeah.net>
2017-10-30 12:35:25 +0800

    change in server

--- analyzer/main.py
+++ analyzer/main.py
@@ -7,11 +7,7 @@ import MySQLdb
 
 import utils
 
-<<<<<<< HEAD
-conn_code = MySQLdb.connect(host="localhost",user="root",passwd="111111",db="codepedia")
-=======
-conn_code = MySQLdb.connect(host="10.107.10.130",user="root",passwd="111111",db="code_pedia")
->>>>>>> 099440163dd4b530d10f3e63414bea31de5d4e3c
+conn_code = MySQLdb.connect(host="localhost",user="root",passwd="111111",db="code_pedia")
 cursor_code = conn_code.cursor()
 conn_code.autocommit(False)
 
@@ -35,6 +31,7 @@ def createTable(table_name):
           "`effort` int(11) DEFAULT NULL, " \
           "`status` varchar(20) DEFAULT NULL, " \
           "`severity` varchar(10) DEFAULT NULL, " \
+	  "`has_question` int(2) DEFAULT 0, " \
           "PRIMARY KEY (`id`), " \
           "KEY `project_id` (`project_id`), " \
           "KEY `blob_id` (`blob_id`), " \
@@ -55,7 +52,7 @@ def moveResult(project_id,projectKey,projectName):
 
     # 读取当前项目function_lineNum和blob_lineNum的对应关系
     lineNumMap = {}
-    cursor_code.execute("select blob_linenum,function_linenum,function_id,blob_id from `lines` where project_id=%s",(project_id,))
+    cursor_code.execute("select file_linenum,function_linenum,function_id,file_id from `projects_line` where project_id=%s",(project_id,))
     items = cursor_code.fetchall()
     for item in items:
         blob_linenum = item[0]
@@ -67,7 +64,7 @@ def moveResult(project_id,projectKey,projectName):
 
 
     # 读取当前project_id对应blobs表中所有的文件
-    cursor_code.execute("select id,path from blobs where project_id=%s",(project_id,))
+    cursor_code.execute("select id,path from projects_file where project_id=%s",(project_id,))
     blobs = cursor_code.fetchall()
     blobMap = {} # 用于存储当前项目的blob路径和id对应关系
     if len(blobs) == 0:
@@ -103,13 +100,15 @@ def moveResult(project_id,projectKey,projectName):
         print "没有读取到当前项目在sonar数据库中的信息"
         return None
     project_uuid = project[0]
+    print "project_uuid=%s" % (project_uuid)    
 
     # 读取project对应的所有文件级别的component_uuid
     cursor_sonar.execute("select uuid,path from projects where project_uuid=%s and scope=%s",(project_uuid,"FIL"))
-    items = cursor_sonar.fetchall()
-    for item in items:
-        component_uuid = item[0]
-        component_path = item[1]
+    items_projects = cursor_sonar.fetchall()
+    print "%s files in this project have issues" % (len(items))
+    for item_project in items_projects:
+        component_uuid = item_project[0]
+        component_path = item_project[1]
         if component_path != None and str(component_path).find(projectName) >= 0:
             component_path = component_path[len(projectName):] # 获取文件的相对路径
         else:
@@ -124,6 +123,7 @@ def moveResult(project_id,projectKey,projectName):
         # 读取issues表对应的数据
         cursor_sonar.execute("select id,kee,rule_id,severity,message,line,status,effort,created_at,updated_at,issue_creation_date,issue_update_date,tags,component_uuid,issue_type from issues where component_uuid=%s",(component_uuid,))
         items = cursor_sonar.fetchall()
+        print "this file has %s issues" % (len(items))
         for item in items:
             id = item[0]
             kee = item[1]
@@ -167,6 +167,8 @@ def moveResult(project_id,projectKey,projectName):
 
 
 if __name__ == "__main__":
+
+    createTable(sonar_table)
 
     while True:
 
@@ -244,8 +246,8 @@ if __name__ == "__main__":
 
                 # 转存数据
                 moveResult(project_id, projectKey, projectName)
-
-        for project_id in projectIds:
+        
+         for project_id in projectIds:
             # 查看sonar_results表中是不是有对应的数据
             cursor_code.execute("select * from sonar_results where project_id=%s",(project_id,))
             if len(cursor_code.fetchall()) == 0:
@@ -255,15 +257,5 @@ if __name__ == "__main__":
                 projectName = "project:" + str(project_id)
                 moveResult(project_id,projectKey,projectName)
 
-        for project_id in projectIds:
-            # 查看sonar_results表中是不是有对应的数据
-            cursor_code.execute("select * from sonar_results where project_id=%s",(project_id,))
-            if len(cursor_code.fetchall()) == 0:
-                # 表示没有对应的记录
-                print "项目%s转存数据库出现了问题" % (project_id)
-                projectKey = "project:" + str(project_id)
-                projectName = "project:" + str(project_id)
-                moveResult(project_id,projectKey,projectName)
-
-        print "处理完了一批工程,休眠1000秒..."
-        time.sleep(1000) # 表示每次处理之后休眠1000s
+        print "处理完了一批工程,休眠100秒..."
+        time.sleep(100) # 表示每次处理之后休眠1000s
